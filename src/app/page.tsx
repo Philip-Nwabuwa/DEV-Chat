01f6e58cbc533c6c8501113916fbfb4a5007e41e
Philip Nwabuwa <philipnwabuwa@gmail.com>
2023-04-20 14:41:04 +0100

    added Google Next-Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import { db } from "@/lib/db";
+import Button from "@/components/ui/Button";
+// import { db } from "@/lib/db";
 
 export default async function Home() {
-  await db.set("hello", "hello");
-
-  return <div className="m-4">hello world</div>;
+  return <Button>Hello</Button>;
 }
