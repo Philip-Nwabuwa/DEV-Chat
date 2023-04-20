cf708d1c4754e40977c1f09a18f31575ab8f6c77
Philip Nwabuwa <philipnwabuwa@gmail.com>
2023-04-20 14:14:28 +0100

    Linked Redis database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
+import { db } from "@/lib/db";
+
+export default async function Home() {
+  await db.set("hello", "hello");
+
   return <div className="m-4">hello world</div>;
 }
